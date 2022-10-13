704d0a93a38ce1b9bfb070c21dfa085e86efebc6
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-13 17:46:12 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]')
 
+let myInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const getColor = () => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+}
+
+const onClickButtonStart = (event) => {
+    startButton.disabled = true;
+    myInterval = setInterval(getColor, 500)
+}
+
+startButton.addEventListener('click', onClickButtonStart);
+
+const onClickButtonStop = () => {
+    clearInterval(myInterval)
+    startButton.disabled = false;
+}
+
+stopButton.addEventListener('click', onClickButtonStop);
