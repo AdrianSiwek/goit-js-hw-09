6a49ca2201d29ed55a2a20d59b8102029754cc54
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-13 22:32:06 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -47,8 +47,15 @@ function convertMs(ms) {
   // Remaining seconds
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
 }
+
+const startTimer = () => {
+    startBtn.disabled = true;
+    Notify.info("Czas odpalać");
+}
+
+
 
 const addLeadingZero = (value) => {
     return String(value).padStart(2, '0');
