a53d2e668bfb1c396e1cf0582656d806223a339c
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-13 19:06:45 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -14,3 +14,19 @@ let myInterval = null;
 let previousDate = new Date();
 let chosenDate;
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if (selectedDates[0] < previousDate) {
+          startBtn.disabled = true;
+          Notify.warning("Proszę wybrać datę w przyszłości");
+      } else if (selectedDates[0] > previousDate) {
+          startBtn.disabled = false;
+          Notify.success("Data jest poprawna. Rozpoczynamy odliczanie.");
+          chosenDate = selectedDates[0].getTime();
+      }
+  },
+};
