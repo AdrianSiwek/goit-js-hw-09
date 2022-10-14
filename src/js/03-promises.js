109a3fde7dcea5e06b7f9e39f25f470fc1bfcd8d
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-14 23:07:04 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,28 +5,39 @@ const inDelay = document.querySelector('input[name="delay"]');
 const inStep = document.querySelector('input[name="step"]');
 const inAmount = document.querySelector('input[name="amount"]');
 
-form.addEventListener('submit', submitHandler);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve({position, delay});
+        resolve({ position, delay });
         // Fulfill
       } else {
-        reject({position, delay})
+        reject({ position, delay });
         // Reject
       }
-    }, delay)
+    }, delay);
   });
 }
 
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+form.addEventListener("submit", handleSubmit);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let standDelay = Number(inDelay.value);
+  let standStep = Number(inStep.value);
+  let standAmount = Number(inAmount.value);
+
+  for (let i=1, i<=standAmount, i++) {
+    createPromise(i, standDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    standDelay += standStep;
+  }
+}
