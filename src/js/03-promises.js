a5f7e9f7d573691927c1b01d4f19e1f036f8b581
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-14 22:27:12 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
+import Notiflix, { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+const inDelay = document.querySelector('input[name="delay"]');
+const inStep = document.querySelector('input[name="step"]');
+const inAmount = document.querySelector('input[name="amount"]');
+
+form.addEventListener('submit', submitHandler);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({position, delay});
+        // Fulfill
+      } else {
+        reject({position, delay})
+        // Reject
+      }
+    },)
+  });
 }
